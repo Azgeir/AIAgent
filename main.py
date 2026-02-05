e6823b8e6d9c59faa75eedf0d49e32444c858847
Azgeir <askewulf@gmail.com>
2026-02-06 00:14:20 +0100

    AIAgent started

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key == None:
+    raise RuntimeError("API key missing")
+client = genai.Client(api_key = api_key)
+parser = argparse.ArgumentParser(description="AIAgent")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+message = client.models.generate_content(
+            model='gemini-2.5-flash', 
+            contents=messages
+            )
+if message.usage_metadata == None:
+    raise RuntimeError("Metadata is not available")
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {message.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {message.usage_metadata.candidates_token_count}")
+print(message.text)
