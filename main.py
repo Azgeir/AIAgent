e1e6c0eac816815c2292a19021bec959461ab371
Azgeir <askewulf@gmail.com>
2026-02-10 19:13:48 +0100

    Finished the project

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 import argparse
+import sys
 from dotenv import load_dotenv
 from prompts import system_prompt
 from google import genai
 from google.genai import types
 from functions.call_function import call_function, available_functions
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -16,28 +18,40 @@ parser.add_argument("user_prompt", type=str, help="User prompt")
 parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args = parser.parse_args()
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-message = client.models.generate_content(
-            model='gemini-2.5-flash', 
-            contents=messages,
-            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0)
-            )
-if message.usage_metadata == None:
-    raise RuntimeError("Metadata is not available")
-if args.verbose:
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {message.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {message.usage_metadata.candidates_token_count}")
-function_results= []
-if message.function_calls != None:
-    for function_call in message.function_calls:
-        function_call_result = call_function(function_call)
-        if len(function_call_result.parts) == 0:
-            raise Exception("Function call list is empty")
-        if not function_call_result.parts[0].function_response:
-            raise Exception("FunctionResponse is None")
-        if not function_call_result.parts[0].function_response.response:
-            raise Exception("FunctionResponse.response is None")
-        function_results += function_call_result.parts[0]
-        print(f"-> {function_call_result.parts[0].function_response.response}")
+for i in range(20):
+    message = client.models.generate_content(
+                model='gemini-2.5-flash', 
+                contents=messages,
+                config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt, temperature=0)
+                )
+    if message.candidates:
+        for candidate in message.candidates:
+            messages.append(candidate.content)
+    if message.usage_metadata == None:
+        raise RuntimeError("Metadata is not available")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {message.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {message.usage_metadata.candidates_token_count}")
+    function_results= []
+    if message.function_calls != None:
+        for function_call in message.function_calls:
+            function_call_result = call_function(function_call)
+            if len(function_call_result.parts) == 0:
+                raise Exception("Function call list is empty")
+            if not function_call_result.parts[0].function_response:
+                raise Exception("FunctionResponse is None")
+            if not function_call_result.parts[0].function_response.response:
+                raise Exception("FunctionResponse.response is None")
+            function_results.append(function_call_result.parts[0])
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+    else:
+        print(message.text)
+        break
+    if function_results:
+        messages.append(types.Content(role="user", parts=function_results))
+
 else:
-    print(message.text)
+    print("Ran out of iterations before getting a final answer")
+    sys.exit(1) 
+
